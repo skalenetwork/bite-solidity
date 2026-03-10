1518b9960e6265a52ca563a97f61d564acf0f96d
Eduardo Vasques <eduardo.vasques@skalelabs.com>
2026-03-10 11:43:21 +0000

    udpate pragma of LegacyTypes

--- contracts/LegacyTypes.sol
+++ contracts/LegacyTypes.sol
@@ -22,7 +22,7 @@
 // This file is developed for using with old solidity versions.abi
 // solhint-disable compiler-version
 
-pragma solidity >=0.5.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 
 /// @title Types Library
