b99a266ddbc47b663a5cbf5a8dc1855659d0cbe8
Vadim Yavorsky <yavrsky@gmail.com>
2026-04-21 15:46:13 +0100

    add legacy Types library for compatibility with old Solidity versions

---
+++ contracts/v0.5/types.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/**
+ *   v0.5/types.sol - bite-solidity
+ *   Copyright (C) 2026-Present SKALE Labs
+ *   @author Dmytro Stebaiev
+ *
+ *   bite-solidity is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   bite-solidity is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with bite-solidity.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// This file is developed for using with old solidity versions.abi
+// solhint-disable compiler-version
+
+pragma solidity >=0.5.0 <0.6.0;
+
+
+/// @title Types Library
+/// @author Dmytro Stebaiev
+/// @notice Library for types used in BITE contracts
+/// @dev Don't use this library for modern solidity versions
+/// @dev This is legacy version of the types.sol
+library Types {
+    struct PublicKey {
+        bytes32 x;
+        bytes32 y;
+    }
+}
