f33c53019854d1fcdb67aa08b7de4648bf7d2df2
Vadim Yavorsky <yavrsky@gmail.com>
2026-04-23 15:04:40 +0100

    refactor: update file naming in IBiteSupplicant.sol for clarity

--- contracts/v0.5/interfaces/IBiteSupplicant.sol
+++ contracts/v0.5/interfaces/IBiteSupplicant.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 /**
- *   v0.5/interfaces/IBiteSupplicant.sol - bite-solidity
+ *   IBiteSupplicant.sol - bite-solidity
  *   Copyright (C) 2026-Present SKALE Labs
  *   @author Dmytro Stebaiev
  *
